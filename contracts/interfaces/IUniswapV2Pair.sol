ac3b127173c62313fbf99c47741a94003d2e1c36
AbZe628 <215593336+AbZe628@users.noreply.github.com>
2026-02-25 10:06:34 -0500

    deploy: Final verified fix

--- contracts/interfaces/IUniswapV2Pair.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-interface IUniswapV2Pair {
-    function approve(address spender, uint256 value) external returns (bool);
-    function balanceOf(address owner) external view returns (uint256);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-}
--- contracts/interfaces/IUniswapV2PairLite.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-// Minimalni Uniswap V2 pair interface koji nam treba za manual burn/mint
-interface IUniswapV2PairLite {
-    function burn(address to) external returns (uint256 amount0, uint256 amount1);
-    function mint(address to) external returns (uint256 liquidity);
-}
